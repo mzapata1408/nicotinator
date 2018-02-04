cdc365b6ce11a472f980f5b41e2c458efaa42c99
Cesar Zapata <cesar.zapata@blis.com>
2018-02-04 12:31:25 +0000

    Creates SpringBoot archetype

---
+++ app/src/main/java/com/nicotinator/nicotinator/NicotinatorApplication.java
@@ -0,0 +1,12 @@
+package com.nicotinator.nicotinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicotinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicotinatorApplication.class, args);
+	}
+}
