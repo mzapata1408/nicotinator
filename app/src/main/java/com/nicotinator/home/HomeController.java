84a77739651ac178130b2e07e48c1c994397c082
Cesar Zapata <cesarzapata1212@gmail.com>
2018-02-05 23:26:53 +0000

    Setup for MVC

--- app/src/main/java/com/nicotinator/home/HomeController.java
+++ app/src/main/java/com/nicotinator/home/HomeController.java
@@ -1,17 +1,15 @@
 package com.nicotinator.home;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("home")
-@Controller
+@RequestMapping("/home")
+@RestController
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        return "home";
+    @GetMapping("greetings")
+    public String greetings(){
+        return "greetings";
     }
-
-
 }
