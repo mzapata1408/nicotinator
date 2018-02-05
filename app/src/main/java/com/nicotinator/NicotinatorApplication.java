066545dd2a715d669a6b91b13cbbd6d9ef01a8c8
Cesar Zapata <cesarzapata1212@gmail.com>
2018-02-05 22:27:58 +0000

    Set up database connection

--- app/src/main/java/com/nicotinator/NicotinatorApplication.java
+++ app/src/main/java/com/nicotinator/NicotinatorApplication.java
@@ -1,4 +1,4 @@
-package com.nicotinator.nicotinator;
+package com.nicotinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
