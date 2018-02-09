095c2aba7cec1920d6cdeee917206e7b57e64cb8
Marco Zapata <marcozapata1408@gmail.com>
2018-02-09 23:02:14 +0000

    Created recipe table

--- app/src/main/resources/database/scripts/initial-schema.sql
+++ app/src/main/resources/database/scripts/initial-schema.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE nicotinatordb;
-
+CREATE TABLE recipe (
+	id serial UNIQUE,
+	name varchar(255) NOT NULL PRIMARY KEY,
+	target_nicotine smallint NOT NULL,
+	target_pg smallint NOT NULL,
+	target_vg smallint NOT NULL,
+	base_nicotine smallint NOT NULL,
+	quantity smallint
+);
