de982818d0a3c21be9101995490823ae051bc2ac
Cesar Zapata <cesarzapata1212@gmail.com>
2018-03-16 00:05:37 +0000

    Change the tables format.

--- app/src/main/java/com/nicotinator/Application.java
+++ app/src/main/java/com/nicotinator/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NicotinatorApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NicotinatorApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- app/src/main/resources/database/scripts/initial-schema.sql
+++ app/src/main/resources/database/scripts/initial-schema.sql
@@ -1,17 +1,36 @@
 CREATE TABLE recipe (
-	id serial UNIQUE,
-	name varchar(255) NOT NULL PRIMARY KEY,
-	target_nicotine smallint NOT NULL,
-	target_pg smallint NOT NULL,
-	target_vg smallint NOT NULL,
-	base_nicotine smallint NOT NULL,
-	quantity smallint
-);
+  id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+  name varchar(255) UNIQUE NOT NULL,
+  target_nicotine smallint(6) NOT NULL,
+  target_pg smallint(6) NOT NULL,
+  target_vg smallint(6) NOT NULL,
+  base_nicotine smallint(6) NOT NULL,
+  quantity smallint(6) DEFAULT NULL,
+  PRIMARY KEY (id),
+) ENGINE=InnoDB;
 
-CREATE TABLE recipe_flavourings (
-    recipe_id serial UNIQUE NOT NULL,
-    flavouring_id int NOT NULL PRIMARY KEY,
-    percentage decimal(2, 2) NULL,
-    FOREIGN KEY (recipe_id)
-    REFERENCES recipe (id)
-);
+DROP TABLE IF EXISTS flavouring;
+CREATE TABLE flavouring (
+    id int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (id),
+    INDEX (name)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS recipe_flavouring;
+CREATE TABLE recipe_flavouring (    
+    recipe_id int(11) NOT NULL,
+    flavouring_id int(11) NOT NULL,
+    percentage decimal(2, 2) NOT NULL,
+    PRIMARY KEY(recipe_id, flavouring_id),
+    INDEX (recipe_id, flavouring_id)
+) ENGINE=INNODB;
+
+ALTER TABLE recipe_flavouring
+ADD CONSTRAINT fk_recipe_flavouring_recipe_id
+FOREIGN KEY (recipe_id) REFERENCES recipe(id);
+
+ALTER TABLE recipe_flavouring
+ADD CONSTRAINT fk_recipe_flavouring_flavouring_id
+FOREIGN KEY (flavouring_id) REFERENCES flavouring(id);
